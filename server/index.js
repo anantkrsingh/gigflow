793dd4f5f9ca63aae7b2167b20625af7eeb1ec0b
anantkrsingh <anantkrplay@gmail.com>
2026-01-12 11:35:24 +0530

    fix: cors origin

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ dotenv.config();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://gigflow-delta.vercel.app"],
     credentials: true,
   })
 );
