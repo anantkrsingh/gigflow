84b9501e8474ced876d8548645c1960f5bbf2269
anantkrsingh <anantkrplay@gmail.com>
2026-01-12 11:51:32 +0530

    fix: cross site cookies

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -38,7 +38,7 @@ exports.login = async (req, res) => {
     res.cookie("token", token, { 
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: "none",
     });
     return res.status(200).json({ 
       message: "Login successful",
